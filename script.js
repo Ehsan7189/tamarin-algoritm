093f95ecb144779d501074f9c03af1b9abbde620
ehsan sayyadi <ehsansayadi7189@gamil.com>
2024-11-10 13:32:56 +0330

    feat:Completion of the first exercise

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-let a = 0;
-let b = 0;
-let c = 0;
-let d = 0;
+let a = 4;
+let b = 3;
+let c = 2;
+let d = 1;
+
+const calculation = (num1,num2,num3,num4) =>{
+    return num1>num3+num4 ? num1*num3 : num2*num4;
+}
+
+
+(function showResult(){
+    console.log(calculation(a,b,c,d));
+    
+})()

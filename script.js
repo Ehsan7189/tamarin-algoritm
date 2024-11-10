a7bcfeb3303033f1b8a6bc45eaf08570ad6741ca
ehsan sayyadi <ehsansayadi7189@gamil.com>
2024-11-10 22:06:19 +0330

    fear:Completion of exercise 2

--- script.js
+++ script.js
@@ -1,14 +1,50 @@
-let a = 4;
-let b = 3;
-let c = 2;
-let d = 1;
+// *tamrin1
+// let a = 4;
+// let b = 3;
+// let c = 2;
+// let d = 1;
+// const calculation = (num1,num2,num3,num4) =>{
+//     return num1>num3+num4 ? num1*num3 : num2*num4;
+// }
 
-const calculation = (num1,num2,num3,num4) =>{
-    return num1>num3+num4 ? num1*num3 : num2*num4;
+
+// (function showResult(){
+//     console.log(calculation(a,b,c,d));
+
+// })()
+// -------------------------------------------------------------------------------
+
+// const a = ( ) => {
+
+//     let array=[] ;
+//     for (let i = 0; i>=500; i++) {
+
+//         array[i] = i
+
+//     }
+//     return array;
+// }
+// a1 = a()
+// console.log(a1);
+// # Multiplier 2 from 1 to 1000
+let nums  = []
+for (let i = 1; i <= 500; i++) {
+    nums.push(i*2);
 }
+console.log(nums);
 
 
-(function showResult(){
-    console.log(calculation(a,b,c,d));
-    
-})()
+
+// function factorialize(num) {
+//     var result = num;
+//     if (num === 0 || num === 1)
+//         return 1;
+//     while (num > 1) {
+//         num--;
+//         result *= num;
+//     }
+//     return result;
+// }
+// // console.log(factorialize(5));
+
+// f
